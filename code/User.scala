78d4b629fe0f0a5f28e1034311ec6c5ee11ce3a5
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-03-05 16:40:21 +0900

    if式の基本構文の練習
    ・if式を用いた基本的な式を書き、sbt console にて動作確認を行いました。

--- code/User.scala
+++ code/User.scala
@@ -2,4 +2,11 @@ class User(val name: String, val age: Int)
 
 object User {
   def printUser(user: User) = println(user.name + "さんは、" + user.age + "才です")
+
+  def adult_or_child(user: User) =
+  if(user.age < 20) {
+    println("未成年です")
+  } else {
+    println("成人です")
+  }
 }
