f339a7723cc700fc0e4578dc6779c65abf98a8bf
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-03-07 11:43:06 +0900

    classに関する基礎構文②
    ・複数の引数リストの扱いと、継承に関する記述を追加し、動作確認を行いました。

--- code/Class.scala
+++ code/Class.scala
@@ -14,5 +14,41 @@ object Point {
     val second = new Point(8, 5)
     println(first + second)
   }
-  
+
+}
+
+
+class Adder {
+
+  def add(first:Int)(second:Int): Int = first + second
+
+}
+
+object Adder {
+
+  def addTest(): Unit = {
+    val adder = new Adder()
+    println(adder.add(2)(3))
+    val lambda = adder.add(2) _
+    println(lambda(3))
+  }
+
+}
+
+// ↓ 呼び出しは new クラス名().メソッド名
+
+class Movie() {
+
+  def print(): Unit = {
+    println("Movie!")
+  }
+
+}
+
+class Animation() extends Movie {
+
+  override def print(): Unit = {
+    println("Animation movie!")
+  }
+
 }
